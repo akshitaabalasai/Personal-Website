1f5a034009e96b31a472d1fb8c4bc7a394f32394
akshitaabalasai <akahitaab@gmail.com>
2024-06-11 19:35:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Simulate a delay for demonstration
-    setTimeout(function() {
-        document.getElementById("loader").style.display = "none";
-        document.getElementById("content").style.display = "block";
-    }, 2000); // Adjust the delay time as needed
-});
+window.onload = function() {
+    var loader = document.getElementById("loader");
+    var content = document.getElementById("content");
+
+    if (loader && content) {
+        loader.style.display = "none";
+        content.style.display = "block";
+    }
+};
