e155affc0db648db4fd897ca9a2a107ad5a66b7e
akshitaabalasai <akahitaab@gmail.com>
2024-06-11 19:31:19 -0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Simulate a delay for demonstration
-    setTimeout(function() {
-        document.getElementById("loader").style.display = "none";
-        document.getElementById("content").style.display = "block";
-    }, 2000); // Adjust the delay time as needed
-});
